e77b316ed6563955c1f0c23f8cc1437089d3fd59
Imtiyazdeveloper <imtiyazmd803@gmail.com>
2023-01-04 00:57:12 +0530

    add

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -1,13 +1,22 @@
-import React from 'react';
+<<<<<<< HEAD
+import React, {useState} from 'react';
 
 const ThemeContext = React.createContext()
 const ThemeProvider = (props) =>{
+    const [theme, setTheme]=useState('light');
+=======
+import React, { useState } from 'react';
 
+const ThemeContext = React.createContext()
+const ThemeProvider = (props) =>{
+     const [theme, setTheme] = useState('light')
+>>>>>>> caa9d6e68ceb5acd536a963062ccf7c1840505d3
     return (
         <React.Fragment>
-
+            <ThemeContext.Provider value={{theme, setTheme}}>
+                {props.children}
+            </ThemeContext.Provider>
         </React.Fragment>
     )
 }
-
 export {ThemeProvider,ThemeContext}
